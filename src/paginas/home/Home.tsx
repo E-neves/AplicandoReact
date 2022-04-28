f52dd222cf46139c7a0b49df4c828ff8e463de3d
E-neves <egaabs9@gmail.com>
2022-04-28 16:45:13 -0300

    Aplicando React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';    
+
+function Home(){
+    return(
+    <>
+          <h1 className='titulo'>Esmeralda Neves</h1>
+<img src="https://i.pinimg.com/originals/c5/65/3d/c5653d29690c9d19ead852c95e68d791.png" alt="Imagem Tela Inicial" className="img" />
+      </>
+    );
+}
+
+export default Home; 
