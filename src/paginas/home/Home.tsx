85677a177b16359d4d49e2677bcdcd084003e9c3
E-neves <egaabs9@gmail.com>
2022-04-28 20:38:21 -0300

    Colocando em pratica Grid e FlexBox

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import './Home.css';    
+import {Button, Box, Paper} from '@material-ui/core';
+import './Home.css';
+
 
 function Home(){
     return(
     <>
-          <h1 className='titulo'>Esmeralda Neves</h1>
-<img src="https://i.pinimg.com/originals/c5/65/3d/c5653d29690c9d19ead852c95e68d791.png" alt="Imagem Tela Inicial" className="img" />
-      </>
+         <Paper>
+             <Box p={2}>
+                <Box display="flex" justifyContent="flex-end" >
+                     <h1>TITULO </h1>
+                 </Box>
+                 <img src="https://thumbs.dreamstime.com/b/flores-de-canto-aqua-green-arch-floral-border-da-aquarela-do-quadro-das-cor-rosa-111389285.jpg" alt="IMAGEM DE FUNDO - FLORAL" style={{width: "100%", height: "100%"}} />
+                 <Box display="flex" justifyContent="center" p={2}>
+                     <Button variant="contained" color="primary"> TESTE 1</Button>
+                     <Button variant="contained" color="secondary"> TESTE 2</Button>
+                 </Box>
+             </Box>
+         </Paper>
+    </>
     );
 }
 
